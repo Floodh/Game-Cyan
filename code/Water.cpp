be4cccbe8db5747c1692293734fc1d7f141fd107
Eric Floodh <ericfloodh@gmail.com>
2023-04-07 19:01:03 +0200

    Implemented more complicated water model.     -   Laid the ground work for edventually generating the water model based on data sent in from constructor.     -   The camera is acting weird tho, need to investigate that.

--- code/Water.cpp
+++ code/Water.cpp
@@ -1,35 +1,96 @@
 #include "Water.hpp"
 
+#include <iostream>
+
+//  green(land) 75 105 47
+//  blue(water) 99 155 255
+
 Water::Water(Camera& camera)
     : camera{camera}
 {
 
     this->shader = loadShaders("shader/water.vert", "shader/water.frag");
 
+    //  world data contaisn information about the quads NOT the vertexes
+    int worldHeight = 3;
+    int worldWidth = 3;
+    int vertexHeight = worldHeight + 1;
+    int vertexWidth = worldWidth + 1;
+    GLint* worldData = new GLint[worldHeight * worldWidth * 4]  //  placeholder for testing
+    {
+        99, 155, 255,
+        99, 155, 255,
+        99, 155, 255,
+
+        99, 155, 255,
+        75, 105, 47,
+        99, 155, 255,
+
+        99, 155, 255,
+        99, 155, 255,
+        99, 155, 255
+    };
+
+
 
     //  world generation here
-        this->indexCount = 6;
-        this->vertexCount = 4;    
-        this->valueCount = vertexCount * 3;
+        this->indexCount = worldHeight * worldWidth * 2 * 3;    //  number of quads multiplied by 2 to get number of triangles, multiplied by 3 to get number of indexes for the triangles.
+        this->vertexCount = vertexHeight * vertexWidth;    
+        this->valueCount = vertexCount * 3; //  we have 3 values for each vertex
 
-        //  create a test quad
-        this->vertices = new GLfloat[valueCount]{
-            -0.5, -0.5, -2.0,
-            0.5, -0.5, -1.5,
-            0.5, 0.5, -1.5,
-            -0.5, 0.5, -2.0,
-        };
-        this->colors = new GLfloat[valueCount]{
-            0.0, 0.0, 0.5,
-            1.0, 0.0, 0.5,
-            1.0, 1.0, 0.5,
-            0.0, 1.0, 0.5,
-        };
-        this->indices = new GLint[indexCount]
+        this->indices = new GLint[indexCount];
+        this->vertices = new GLfloat[valueCount];
+        this->colors = new GLfloat[valueCount];
+
+
+        //  initlize all indexes for indicies
         {
-            0, 1, 3,
-            1, 2, 3 
-        };
+        int count = 0;
+        for (int z = 0; z < worldHeight; z++)
+            for (int x = 0; x < worldWidth; x++)
+            {
+                int p1Index = (z * worldWidth) + x;
+                int p2Index = (z * worldWidth) + x + 1;
+                int p3Index = ((z + 1) * worldWidth) + x;
+                int p4Index = ((z + 1) * worldWidth) + x + 1;
+
+                //  this may not be clockwise, this could cause problems with culling and calculating normals
+                this->indices[count++] = p1Index;
+                this->indices[count++] = p2Index;
+                this->indices[count++] = p3Index;
+
+                this->indices[count++] = p2Index;
+                this->indices[count++] = p3Index;
+                this->indices[count++] = p4Index;
+            }
+        if (count != indexCount)
+        {
+            std::cout << "The loaded index values for water generation does not match the indexCount(array size), count = " << count << ", indexCount = " << indexCount << std::endl;
+            exit(1);
+        }
+        }
+
+        //  initlize all vertexes coords and color
+        for (int z = 0; z < vertexHeight; z++)
+            for (int x = 0; x < vertexWidth; x++)
+            {
+                GLfloat vertexX = (GLfloat)x;
+                GLfloat vertexY = (GLfloat)0.0f;
+                GLfloat vertexZ = (GLfloat)z;
+
+                GLfloat r = vertexX / vertexHeight, g = vertexZ / vertexHeight,b = 0.9;    //  placeholder values, will be determined by worldData
+
+
+                int indexOffset = ((z * worldWidth) + x) * 3;
+                this->vertices[indexOffset + 0] = vertexX;
+                this->vertices[indexOffset + 1] = vertexY;
+                this->vertices[indexOffset + 2] = vertexZ;
+
+                this->colors[indexOffset + 0] = r;
+                this->colors[indexOffset + 1] = g;
+                this->colors[indexOffset + 2] = b;
+            }
+
 
     //  
 
--- include/LoadBMP.hpp
+++ include/LoadBMP.hpp
@@ -12,5 +12,5 @@
 //  format should be: red, green, blue, alpha
 //  the size of the .bmp file should be equal to the number of pixels times 4
 //  width and height should get their apropiet values(should be found in the header of the .bmp file)
-//  keep in mind that the values of the pixels are stored in byte, which is to say they are not floats
-GLfloat* LoadBMP(std::string path, int& width, int& height);
+//  it turns out that sending the int values is better than converting them to floats
+GLint* LoadBMP(std::string path, int& width, int& height);
