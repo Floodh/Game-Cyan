4d8e89c8252d7af2cad67c7d00650580145b9c4c
Erik Eklöf <eriek409@student.liu.se>
2023-05-04 21:25:32 +0200

    Made sure the players y-value isnt changed

--- code/NG1.cpp
+++ code/NG1.cpp
@@ -6,7 +6,7 @@ void Game::NG1_Init()
 {
 
     this->portal->SetPosition(5.5, 1.0, 2.5);
-    this->player->setPosition(1.5, 0.75, 8.5);
+    this->player->setPosition(1.5, this->player->getPosition().y, 8.5);
 
 }
 
